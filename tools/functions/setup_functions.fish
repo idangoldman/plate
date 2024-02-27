981a3f14a34925a6efc2b675659d313f40e2c4d7
Idan Goldman <idan@goldman.work>
2024-02-27 16:01:38 +0200

    correct path fix

--- tools/functions/setup_functions.fish
+++ tools/functions/setup_functions.fish
@@ -1,7 +1,7 @@
 #!/usr/bin/fish
 
 # Load package functions into fish function path
-set -l plate_fish_functions "$PLATE_PKG/scripts/functions"
+set -l plate_fish_functions "$PLATE_PKG/tools/functions"
 if not contains $plate_fish_functions $fish_function_path
     set fish_function_path $plate_fish_functions $fish_function_path
 end
