143c1e6abe694cbfb1e641d656a43dc6581fe1a8
Idan Goldman <idan@goldman.work>
2024-03-12 01:41:14 +0200

    theoretical comments

--- imports/globals.mjs
+++ imports/globals.mjs
@@ -1,6 +1,11 @@
 import gulp from "gulp";
 global.gulp = gulp;
 
+// global.PLATE_CONFIG = {};
+// global.PLATE_ENV = process.env.PLATE_ENV || "development";
+// global.PLATE_PKG = process.env.PLATE_PKG || path.join("..");
+// global.PLATE_ROOT = process.env.PLATE_ROT || path.join(process.cwd());
+
 global.PLATE_ENV  = {};
 global.PLATE_PKG  = process.env.PLATE_PKG;
 global.PLATE_ROOT = process.env.PLATE_ROOT;
