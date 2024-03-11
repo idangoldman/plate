cc299d62204a14bad8f3f0d96748087b865a2622
Idan Goldman <idan@goldman.work>
2024-03-11 11:50:54 +0200

    update helpers with some templates info commented

--- tools/slim/helpers.rb
+++ tools/slim/helpers.rb
@@ -13,6 +13,13 @@ module SlimHelpers
     Slim::Template.new("#{PLATE_ROOT}/#{PLATE_ENV.templates.views}/#{path}.slim").render(scope)
   end
 
+  # https://github.com/slim-template/slim/blob/main/doc/include.md
+  # https://github.com/slim-template/slim/blob/main/lib/slim/include.rb
+  # https://github.com/slim-template/slim/blob/main/lib/slim/interpolation.rb
+  # def include_slim(name, options = {}, &block)
+  #   Slim::Template.new("#{name}.slim", options).render(self, &block)
+  # end
+
   def class_if(condition, class_name)
     condition ? class_name : nil
   end
