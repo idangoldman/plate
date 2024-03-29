0868650d2dd319ef5c3b5872a70550d06f22e0db
Idan Goldman <idan@goldman.work>
2024-03-29 17:38:30 +0300

    refactor of Dictionary capture functionality

--- tools/slim/dictionary.rb
+++ tools/slim/dictionary.rb
@@ -2,12 +2,7 @@ module Slim
   class Dictionary < Filter
     def on_slim_output(escape, code, content)
       capture = code.match(/(?<method>^[a-z][a-z\.0-9_]+)(?:\((?<args>.*)\))?/i)
-
-      if capture && I18n.exists?(capture[:method])
-        code = "I18n.t('#{capture[:method]}')"
-
-        code = code.gsub(")", ", #{capture[:args]})") if capture[:args]
-      end
+      code = replace_dots_with_i18n(code, capture) if I18n.exists?(capture[:method])
 
       LOG.info "
         ON_SLIM_OUTPUT
@@ -16,6 +11,16 @@ module Slim
         CONTENT: #{content.inspect}
       "
       [:slim, :output, escape, code, compile(content)]
+    end
+
+    def replace_dots_with_i18n(code, captured)
+      captured => { method:, args: }
+
+      if args.present?
+        code.gsub "#{method}(#{args})", "I18n.t('#{method}', #{args})"
+      else
+        code.gsub method, "I18n.t('#{method}')"
+      end
     end
   end
 end
