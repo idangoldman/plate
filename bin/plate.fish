eb54e5e2b35410a13f22c81133889b081a12ba3b
Idan Goldman <idan@goldman.work>
2024-05-15 20:56:40 +0300

    using a fish function "find_parent_folder" from cmdfile repository of mine to fix upward path finding

--- bin/plate.fish
+++ bin/plate.fish
@@ -1,7 +1,8 @@
 #!/usr/bin/env fish
 
 # Package setup of fish shell environment
-set --global --export PLATE_PKG (realpath (dirname (status --current-filename))/..)
+set --global --export PLATE_PKG (find_parent_folder (status --current-filename) plate)
+set --global --export PLATE_NODE_MODULES (find_parent_folder $PLATE_PKG node_modules)
 set --global --export PLATE_ROOT (pwd -P)
 
 set --local registry_path $PLATE_PKG/imports/registry.mjs
@@ -11,7 +12,7 @@ argparse 'gulp=!test -f "$_flag_value"' -- $argv
 set --local node_arguments $argv
 
 if set --query _flag_gulp
-    set --local gulp_bin "$PLATE_PKG/node_modules/gulp/bin/gulp.js"
+    set --local gulp_bin "$PLATE_NODE_MODULES/gulp/bin/gulp.js"
     set --local gulp_cwd "--cwd='$PLATE_ROOT'"
     set --local gulp_file "--gulpfile='$PLATE_PKG/config/gulpfile.mjs'"
     set --global --export PLATE_GULPFILE (realpath $_flag_gulp)
