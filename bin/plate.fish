0cf7152049a0e6f96477c4ffc36fbdb58480c84f
Idan Goldman <idan@goldman.work>
2024-08-06 18:37:57 +0400

    we can start :)

--- bin/plate.fish
+++ bin/plate.fish
@@ -6,6 +6,16 @@ set --global --export PLATE_NODE_MODULES (find_parent_folder $PLATE_PKG node_mod
 set --global --export PLATE_PRJ (pwd -P)
 
 set --local registry_path $PLATE_PKG/imports/registry.mjs
-set --local node_arguments $argv
+set --local node_arguments ""
 
-node --trace-warnings --import $registry_path $node_arguments
+for argument in $argv
+    if test $argument = locals
+        set argument $PLATE_PKG"bin/$argument.js"
+    end
+
+    set --append node_arguments $argument
+end
+
+echo $node_arguments
+
+# node --trace-warnings --import $registry_path $node_arguments
